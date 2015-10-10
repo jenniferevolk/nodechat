bb7211294135e30884bcf8c2c270a874c25ef86d
Jennifer E. Volk <jenniferevolk@gmail.com>
2015-10-10 16:04:22 -0700

    basic chat with nicks working

--- nodechat.js
+++ nodechat.js
@@ -4,13 +4,25 @@ var server=require('http').Server(app);
 var io=require('socket.io')(server);
 app.use(express.static(__dirname))
 
-//detect connect
+//detect connect/disconnect
 io.on('connect',function(socket){
 	console.log('connected');
-socket.on('disconnect',function(){
-	console.log('disconnected');
+
+	socket.on('disconnect',function(){ 
+		console.log('disconnected');
+	});
+
+//communications
+
+	//client to server
+	socket.on('message', function(msg){
+		console.log('message: '+msg);
+		
+	//server to everyone 
+		 io.emit('message', msg);
 	});
 });
+
 
 
 //serve page
