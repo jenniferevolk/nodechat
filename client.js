c9bf01360e93090ebb7ab1a659f2353391753d30
Jennifer E. Volk <jenniferevolk@gmail.com>
2015-10-18 11:40:22 -0700

    ui: added scrolling chat text window

--- client.js
+++ client.js
@@ -1,17 +1,18 @@
 var socket=io();
 var nickname;
-
+var $messages=$('.messages')
 socket.on('connect',function(){
 
 	//get username and join chat
-	$('#status').html('you are now connected');
 	nickname=prompt('what is your nickname?');
+	$('#status').html('you are now connected');
 	socket.emit('user joined',nickname);
 
 	//receive message
 	socket.on('message',function(message){
 	console.log(message);
 	$('#messages').append($('<ul>').text(message));
+	$('#messages').animate({scrollTop: $('#messages').height()});
 	});
 
 	//send message
