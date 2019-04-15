49864f4a973b705a18399a66b48999f86ae67382
jennifer e. volk <jenniferevolk@gmail.com>
2019-04-15 14:59:25 -0700

    jss changes: fixed unnecessary/needed semicolons, neatened spacing on code on client and server js, changed global isare variable to local in client.js

--- client.js
+++ client.js
@@ -1,153 +1,166 @@
-var socket=io();
+var socket = io();
 var nickname;
-var userlist=[];
-var textarea=$('#send');
-var usersTyping=[];
-var typing=0;
+var userlist = [];
+var textarea = $('#send');
+var usersTyping = [];
+var typing = 0;
 var lastTypedTime;
 
 
-socket.on('connect',function(){
-
-	
-	//receive userlist
-	socket.on('userlist',function(list){
-		$('#status').html('you are now connected');
-		userlist=list;
-		updateUserList();
-	});
-
-	socket.on('username',function(username){
-		nickname=username
-		$('#nick').val(username);
-		
-	});
-	socket.on('rename',function(data){
-		userlist[userlist.indexOf(data.old)]=data.new;
-		updateMessages("",data.old+" is now known as "+data.new,1);
-		updateUserList();
-	});
-
-	//add user
-	socket.on('add',function(username){
-		userlist.push(username);
-		updateUserList();
-		if(username!==nickname){
-		updateMessages("",username+" has joined.",1);
-		};
-	});
+socket.on('connect', function() {
 
 
-	//remove user
-	socket.on('remove',function(username){
-		userlist.splice(userlist.indexOf(socket.username),1);
-		updateUserList();
-		updateMessages("",username+" has left.",1);
-	});
+  //receive userlist
+  socket.on('userlist', function(list) {
+    $('#status').html('you are now connected');
+    userlist = list;
+    updateUserList();
+  });
 
-	//receive message
-	socket.on('message',function(data){
-		updateMessages(data.user,data.message);
-	});
+  socket.on('username', function(username) {
+    nickname = username;
+    $('#nick').val(username);
 
-	//send message
-	$('#send').keypress(function(event){
-		if (event.which==13){
-			socket.emit('message',$('#send').val())
-			$("#send").val("");
-			event.preventDefault();
-		}
-});
-	
-	//Get nickname
-	$('#nick').keypress(function(event){
-		if(event.which==13){
-			nickname=$("#nick").val();
-			socket.emit('rename',nickname);
-			event.preventDefault();
-		}
-	});
+  });
+  socket.on('rename', function(data) {
+    userlist[userlist.indexOf(data.old)] = data.new;
+    updateMessages("", data.old + " is now known as " + data.new, 1);
+    updateUserList();
+  });
+
+  //add user
+  socket.on('add', function(username) {
+    userlist.push(username);
+    updateUserList();
+    if (username !== nickname) {
+      updateMessages("", username + " has joined.", 1);
+    }
+  });
 
 
-	//another person is typing
-	socket.on('typing on',function(username){
-		if (username!=nickname){typingMessageON(username)};
-	});
+  //remove user
+  socket.on('remove', function(username) {
+    userlist.splice(userlist.indexOf(socket.username), 1);
+    updateUserList();
+    updateMessages("", username + " has left.", 1);
+  });
 
-	//another person stopped typing
-	socket.on('typing off',function(username){
-		if ($.inArray(username,usersTyping)>-1 && username!=nickname){
-			typingMessageOFF(username);
-		}
-	});
+  //receive message
+  socket.on('message', function(data) {
+    updateMessages(data.user, data.message);
+  });
+
+  //send message
+  $('#send').keypress(function(event) {
+    if (event.which == 13) {
+      socket.emit('message', $('#send').val());
+      $("#send").val("");
+      event.preventDefault();
+    }
+  });
+
+  //Get nickname
+  $('#nick').keypress(function(event) {
+    if (event.which == 13) {
+      nickname = $("#nick").val();
+      socket.emit('rename', nickname);
+      event.preventDefault();
+    }
+  });
 
 
-//watch for keypresses and refesh our typing status
-setInterval(refreshTypingStatus,100);
-textarea.keypress(updateLastTypedTime);
-textarea.blur(refreshTypingStatus);
+  //another person is typing
+  socket.on('typing on', function(username) {
+    if (username != nickname) {
+      typingMessageON(username);
+    }
+  });
+
+  //another person stopped typing
+  socket.on('typing off', function(username) {
+    if ($.inArray(username, usersTyping) > -1 && username != nickname) {
+      typingMessageOFF(username);
+    }
+  });
+
+
+  //watch for keypresses and refesh our typing status
+  setInterval(refreshTypingStatus, 100);
+  textarea.keypress(updateLastTypedTime);
+  textarea.blur(refreshTypingStatus);
 
 });
 
 //if we started typing let them know ONCE and set the timer
-function updateLastTypedTime(){
-	if(typing==0){
-		socket.emit('typing on','');
-		typing=1;
-	}
-	lastTypedTime=new Date();
+function updateLastTypedTime() {
+  if (typing == 0) {
+    socket.emit('typing on', '');
+    typing = 1;
+  }
+  lastTypedTime = new Date();
 }
 
 //if we haven't pressed a key for over a second let them know we stopped
-function refreshTypingStatus(){
-	if(typing==1 && new Date().getTime()-lastTypedTime>500){
-		socket.emit('typing off','');
-		typing=0;
-	}
+function refreshTypingStatus() {
+  if (typing == 1 && new Date().getTime() - lastTypedTime > 500) {
+    socket.emit('typing off', '');
+    typing = 0;
+  }
 }
 
-function typingMessageON(username){
-	if(username!=null){usersTyping.push(username)};
-	if(usersTyping.length==0){return};
-	if(usersTyping.length==1){
-		var isAre=" is typing";
-	} else {
-		var isAre=" are typing";
-	}
-	$('#messages').append('<ul id="temporaryMessage"><i>'+usersTyping+isAre+'</i></ul>');		
-	$('#messages').animate({scrollTop: $('#messages').height()});
+function typingMessageON(username) {
+  if (username != null) {
+    usersTyping.push(username);
+  }
+  if (usersTyping.length == 0) {
+    return;
+  }
+  if (usersTyping.length == 1) {
+    isAre = " is typing";
+  } else {
+    isAre = " are typing";
+  }
+  $('#messages').append('<ul id="temporaryMessage"><i>' + usersTyping + isAre + '</i></ul>');
+  $('#messages').animate({
+    scrollTop: $('#messages').height()
+  });
 }
 
 
-function typingMessageOFF(username){
-	if(username!=null){usersTyping.splice(username,1)};
-	$('#temporaryMessage').remove();
+function typingMessageOFF(username) {
+  if (username != null) {
+    usersTyping.splice(username, 1);
+  }
+  $('#temporaryMessage').remove();
 }
 
 
 
-function updateUserList(){
-	var numusers=userlist.length;
-	userlist.sort();
-	$('#userlist').empty();
-	for(var i=0;i<numusers;i++){	
-	$('#userlist').append('<ul>'+userlist[i]+'</ul>');
-	}
-	$('#userlist').animate({scrollTop: $('#userlist').height()});
-};
-function updateMessages(user,message,system){
-		typingMessageOFF();
-		if(system==1){
-		$('#messages').append('<ul><i>'+message+'</i></ul>');
-		} else {		
-		$('#messages').append('<ul><b>'+user+": </b>"+message+'</ul>');
-		};
-		$('#messages').animate({scrollTop: $('#messages').height()});
-		typingMessageON();
-};
+function updateUserList() {
+  var numusers = userlist.length;
+  userlist.sort();
+  $('#userlist').empty();
+  for (var i = 0; i < numusers; i++) {
+    $('#userlist').append('<ul>' + userlist[i] + '</ul>');
+  }
+  $('#userlist').animate({
+    scrollTop: $('#userlist').height()
+  });
+}
 
-socket.on('disconnect',function(){
-	$('#status').html('you are now disconnected');
+function updateMessages(user, message, system) {
+  typingMessageOFF();
+  if (system == 1) {
+    $('#messages').append('<ul><i>' + message + '</i></ul>');
+  } else {
+    $('#messages').append('<ul><b>' + user + ": </b>" + message + '</ul>');
+  }
+  $('#messages').animate({
+    scrollTop: $('#messages').height()
+  });
+  typingMessageON();
+}
+
+socket.on('disconnect', function() {
+  $('#status').html('you are now disconnected');
 });
-
-
--- server.js
+++ server.js
@@ -1,73 +1,76 @@
-var express=require('express');
-var app=express();
-var server=require('http').Server(app);
-var io=require('socket.io')(server);
-app.use(express.static(__dirname))
-var userlist=[];
-var blocked=0;
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.use(express.static(__dirname));
+var userlist = [];
+var blocked = 0;
+
+
 //socket actions
-io.on('connect',function(socket){
-	var users=userlist.length
-	var username="User "+users;
-	var count=0;
-	while(userlist.indexOf(username)>-1){
-		count++;
-		username="User "+count;
-	}
-	userlist.push(username);
-	socket.username=username;
-	socket.emit('username',username);
-	io.emit('add',username);
-	socket.emit('userlist',userlist);
-	console.log(username+" joined.");
+io.on('connect', function(socket) {
+  var users = userlist.length;
+  var username = "User " + users;
+  var count = 0;
+  while (userlist.indexOf(username) > -1) {
+    count++;
+    username = "User " + count;
+  }
+  userlist.push(username);
+  socket.username = username;
+  socket.emit('username', username);
+  io.emit('add', username);
+  socket.emit('userlist', userlist);
+  console.log(username + " joined.");
 
-	
-	//user joined
-	socket.on('rename', function(username){
-		if(userlist.indexOf(username)>-1 || username==null){
-			console.log('BLOCKED: invalid username: '+username);
-			socket.emit('server error','invalid username');	
-			socket.emit('username',socket.username);
-		} else {
 
-			//rename user 
-			console.log(socket.username+" renamed to "+username);
-			userlist[userlist.indexOf(socket.username)]=username;
-			io.emit('rename',{old: socket.username, new: username});
-			socket.username=username;
-		}
-	});
-		
-	//user left
-	socket.on('disconnect',function(username){
-			console.log(username+" left.");
-			userlist.splice(userlist.indexOf(username),1);
-			io.emit('remove',username);
-	});
+  //user joined
+  socket.on('rename', function(username) {
+    if (userlist.indexOf(username) > -1 || username == null) {
+      console.log('BLOCKED: invalid username: ' + username);
+      socket.emit('server error', 'invalid username');
+      socket.emit('username', socket.username);
+    } else {
 
-	//receive and share user message
-	socket.on('message', function(msg){
-		io.emit('message',{user: socket.username, message: msg});
-	});
-	
-	
-	//user typing
-	socket.on('typing on',function(){
-		io.emit('typing on',socket.username);
-	});
+      //rename user
+      console.log(socket.username + " renamed to " + username);
+      userlist[userlist.indexOf(socket.username)] = username;
+      io.emit('rename', {
+        old: socket.username,
+        new: username
+      });
+      socket.username = username;
+    }
+  });
 
-	//user stopped typing
-	socket.on('typing off',function(){
-		io.emit('typing off',socket.username);
-	});	 	
-		
+  //user left
+  socket.on('disconnect', function(username) {
+    console.log(username + " left.");
+    userlist.splice(userlist.indexOf(username), 1);
+    io.emit('remove', username);
+  });
+
+  //receive and share user message
+  socket.on('message', function(msg) {
+    io.emit('message', {
+      user: socket.username,
+      message: msg
+    });
+  });
+
+  //user typing
+  socket.on('typing on', function() {
+    io.emit('typing on', socket.username);
+  });
+
+  //user stopped typing
+  socket.on('typing off', function() {
+    io.emit('typing off', socket.username);
+  });
 });
-
-
 
 //serve page
-app.get('/',function(req,res){
-	res.sendFile('index.html');
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
 });
 server.listen(8080);
-
